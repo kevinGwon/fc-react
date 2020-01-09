7dc72314870746cf2c410f9baade6f2122147c98
kevinGwon <developerGwon@gmail.com>
2020-01-09 21:24:57 +0900

    Add homework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Signin from './pages/Signin';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/signin' components={Signin} />
+          <Route exact path='/' components={Home} />
+          <Route components={NotFound} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Signin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Col } from 'antd';
+
+function Signin() {
+  return (
+    <Col span={12}>
+      <img src={bg} style={{ width: '100%' }} alt='Signin' />
+    </Col>
+  );
+}
+
+export default Signin;
